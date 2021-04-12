1d92f6facfa36cd98cb3b853f6d3dcc6b7e5eb4c
Loc Le <locle.isme@gmail.com>
2021-04-12 15:38:12 +0700

    setup report event

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,6 +10,20 @@ class ReportController extends Controller
     //
     public function index(Event $event)
     {
+        $event->sessions = $event->rooms->map(function ($room) {
+            return $room->sessions->map(function ($session) use ($room) {
+                $session->start = date('H:i', strtotime($session->start));
+                $session->end = date('H:i', strtotime($session->end));
+                $session->capacity = $room->capacity;
+                if ($session->type == 'talk') {
+                    $session->attendee = $room->channel->event->registrations->count();
+                } else {
+                    $session->attendee = $session->sessionRegistrations->count();
+                }
+                return collect($session)->only('title', 'start', 'end', 'capacity', 'attendee');
+            });
+        })->collapse()->sortBy('start')->values();
+        //dd($event->sessions);
         return view('reports.index', compact('event'));
     }
 }
