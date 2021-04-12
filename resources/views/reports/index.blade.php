19e325a893f06d714722602f159f3576d1cba3f2
Loc Le <locle.isme@gmail.com>
2021-04-12 15:41:19 +0700

    Revert "setup report event"
    This reverts commit 1d92f6facfa36cd98cb3b853f6d3dcc6b7e5eb4c.

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -1,39 +1,33 @@
 @extends('layouts.app')
 @section('content')
-    <canvas id="myChart" width="400" height="200"></canvas>
+    <canvas id="myChart" width="400" height="400"></canvas>
     <script>
-        let reportData = @json($event->sessions);
-        let titles = [];
-        let capacities = [];
-        let attendees = [];
-        let bgColors = [];
-        reportData.forEach(row => {
-            titles.push(row.title);
-            capacities.push(row.capacity);
-            attendees.push(row.attendee);
-            if (row.capacity > row.attendee) {
-                bgColors.push('#41eea6');
-            } else {
-                bgColors.push('#fa3343');
-            }
-        })
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: titles,
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
                 datasets: [{
-                    label: '# of Attendee',
-                    data: attendees,
-                    backgroundColor: bgColors,
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
                     borderWidth: 1
-                },
-                    {
-                        label: '# of Capacity',
-                        data: capacities,
-                        backgroundColor: '#52b6ff',
-                        borderWidth: 1
-                    }]
+                }]
             },
             options: {
                 scales: {
