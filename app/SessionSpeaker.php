4dbcd86ae38be331d92c45e0ab6eb1c69f157ad8
Loc Le <locle.isme@gmail.com>
2021-04-13 09:10:40 +0700

    setup speaker model

--- app/SessionSpeaker.php
+++ app/SessionSpeaker.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SessionSpeakers extends Model
+class SessionSpeaker extends Model
 {
     //
     protected $guarded = [];
--- app/Speaker.php
+++ app/Speaker.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Speaker extends Model
 {
     //
+    protected $guarded = [];
+    public $timestamps = false;
+
+    public function sessionSpeakers()
+    {
+        return $this->hasMany(SessionSpeaker::class);
+    }
 }
